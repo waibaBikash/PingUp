1e41614a94d235d57ac8acbc4cf1be826b681185
waibaBikash <kavrelibikash@gmail.com>
2025-08-13 21:31:52 +1000

    feat: enhance layout with sidebar component and responsive menu toggle

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import { Outlet } from 'react-router-dom'
+import { Menu, X } from 'lucide-react'
+import { dummyUserData } from '../assets/assets'
 
 const Layout = () => {
+
+  const user = dummyUserData
+  const [ sidebarOpen, setSidebarOpen ] = useState(false)
   return (
-    <div>
-      
+    <div className='w-full flex h-screen'>
+       <Sidebar />
+         <div className='flex-1 bg-slate-50'>
+           <Outlet />
+         </div>
+
+         {
+           sidebarOpen ? 
+           <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 text-gray-600 sm:hidden' onClick={()=> setSidebarOpen(false)}/>
+           :
+           <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=> setSidebarOpen(true)}/>
+         }
     </div>
   )
 }
