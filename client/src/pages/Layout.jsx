94388790e957b339d5fed7dee431bd0b84da00d4
waibaBikash <kavrelibikash@gmail.com>
2025-08-13 21:37:12 +1000

    feat: add Loading component for improved user experience during data fetching

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -3,12 +3,13 @@ import Sidebar from '../components/Sidebar'
 import { Outlet } from 'react-router-dom'
 import { Menu, X } from 'lucide-react'
 import { dummyUserData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const Layout = () => {
 
   const user = dummyUserData
   const [ sidebarOpen, setSidebarOpen ] = useState(false)
-  return (
+  return user ? (
     <div className='w-full flex h-screen'>
        <Sidebar />
          <div className='flex-1 bg-slate-50'>
@@ -22,6 +23,8 @@ const Layout = () => {
            <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=> setSidebarOpen(true)}/>
          }
     </div>
+  ) : (
+   <Loading/>
   )
 }
 
