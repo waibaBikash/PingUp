07e9f0d596fabf15288e61e5c7ed03490a43f138
waibaBikash <kavrelibikash@gmail.com>
2025-08-14 14:41:05 +1000

    feat: implement Sidebar component with navigation and responsive design

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
-const Sidebar = () => {
+const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
+
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>This is sidebar</h1>
+    <div className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
+      <div className='w-full'>
+         <img onClick={()=> navigate('/')} src={assets.logo} alt="" className='w-26 ml-7 my-2 cursor-pointer' />
+         <hr className='border-gray-300 mb-8' />
+      </div>
     </div>
   )
 }
