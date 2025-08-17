82b5198ca276f88bdc2b1b6b57de2017767667c8
waibaBikash <kavrelibikash@gmail.com>
2025-08-17 21:16:44 +1000

    feat: enhance Sidebar component with user information and logout functionality

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets, dummyUserData } from '../assets/assets'
 import { Link, useNavigate } from 'react-router-dom'
 import MenuItems from './MenuItems';
-import { CirclePlus } from 'lucide-react';
+import { CirclePlus, LogOut } from 'lucide-react';
+import{UserButton, useClerk} from '@clerk/clerk-react';
 
 
 const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
 
   const navigate = useNavigate();
+  const user = dummyUserData;
+  const {signOut} = useClerk();
   return (
     <div className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
       <div className='w-full'>
@@ -22,6 +25,16 @@ const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
               
          
       </div>
+        <div className='w-full border-t border-gray-200 p-4 px-7 flex items-center justify-between '>
+            <div className='flex gap-2 items-center cursor-pointer'>
+              <UserButton />
+               <div>
+                 <h1 className='text-sm font-medium'>{user.full_name}</h1>
+                 <p className='text-sm text-gray-500'>@{user.username}</p>
+               </div>
+            </div>
+              <LogOut onClick={signOut} className='w-4.5 text-gray-400 hover:text-gray-700 transition cursor-pointer'  />
+        </div>
     </div>
   )
 }
