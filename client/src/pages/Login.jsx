299dfdaa09050e1e7bf04fc642aa5df51426c632
waibaBikash <kavrelibikash@gmail.com>
2025-08-19 22:03:44 +1000

    fix: standardize import statements and formatting in Login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { assets } from '../assets/assets'
-import { Star } from 'lucide-react'
-import { SignIn } from '@clerk/clerk-react'
+import { assets } from '../assets/assets';
+import { Star } from 'lucide-react';
+import { SignIn } from '@clerk/clerk-react';
 
 const Login = () => {
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
       {/* Background Image */}
-        <img src={assets.bgImage} alt="" className='absolute top-0 left-0 -z-1 w-full h-full object-cover' />
+        <img 
+        src={assets.bgImage} alt=""
+        className='absolute top-0 left-0 -z-1 w-full h-full object-cover'
+       />
 
       {/* Left Side: Branding */}
       <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:pl-40'>
@@ -17,13 +20,19 @@ const Login = () => {
                   <img src={assets.group_users} alt="" className='h-8 md:h-10' />
                     <div>
                        <div className='flex'>
-                          {Array(5).fill(0).map((_, i)=>(<Star key={{i}} className='size-4 md:size-4.5 text-transparent fill-amber-500'/>))}
+                          {Array(5).fill(0).map((_, i)=>(<Star key={{i}}
+                           className='size-4 md:size-4.5 text-transparent fill-amber-500'/>))}
                        </div>
                        <p>Used by 12k developers</p>
                     </div>
                </div>
-                <h1 className='text-3x md:text-3xl md:pb-2 font-bold bg-gradient-to-r from-indigo-950 to-indigo-800 bg-clip-text text-transparent'>More than just frinds truly connect </h1>
-                <p className='text-xl md:text-3xl text-indigo-900 max-w-72 md:max-w-md'>Connect with global community on pinup</p>
+                <h1 
+                className='text-3x md:text-3xl md:pb-2 font-bold bg-gradient-to-r from-indigo-950 to-indigo-800 bg-clip-text text-transparent'>
+                More than just frinds truly connect 
+                </h1>
+                <p className='text-xl md:text-3xl text-indigo-900 max-w-72 md:max-w-md'>
+                  Connect with global community on pinup
+               </p>
             </div>
             <span className='md:h-10'></span>
       </div>
@@ -36,4 +45,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
