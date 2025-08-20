c77d6af6ffbd6c0f74e3f7f87fd83a0f96f267b4
waibaBikash <kavrelibikash@gmail.com>
2025-08-20 22:38:46 +1000

    feat: add media display for stories in StoriesBar component

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -35,6 +35,19 @@ const StoriesBar = () => {
              <img src={story.user.profile_picture} alt="" className='absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow' />
              <p className='absolute top-8/12 left-3 text-white/60 text-sm truncate max-w-24'>{story.content}</p>
              <p className='text-white absolute bottom-1 right-2 z-10 text-xs'>{moment(story.createdAt).fromNow()}</p>
+             {
+              story.media_type !== 'text' && (
+                 <div className='absolute inset-0 z-1 rounded-lg bg-black overflow-hidden'>
+                   {
+              stories.media_type === "image" ?
+              <img src={story.media_url} alt='' className='h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80'/>
+              : 
+              <video src={story.media_url} className='h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80'/>
+             }
+                 </div>
+              )
+             }
+             
             </div>
            ))
         }
