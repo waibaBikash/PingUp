4294b7a254546eba5a2e8ee9a670688ba1cfe394
waibaBikash <kavrelibikash@gmail.com>
2025-08-24 21:30:13 +1000

    feat: add text and media input options in StoryModel component

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -21,14 +21,14 @@ const StoriesBar = () => {
     <div className='w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl no-scrollbar overflow-x-auto px-4'>
        <div className='flex gap-4 pb-5'>
          {/* Add Story Card */}
-          <div 
+          <div onClick={()=> setShowModal(true)}
            className='rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] hover:shadow-lg transition-all duration-200 border-2 border-dashed border-indigo-300 bg-gradient-to-b from-indigo-50 to-white'>
               <div className='h-full flex flex-col items-center justify-center p-4'>
                 <div className='size-10 bg-indigo-500 rounded-full flex items-center justify-center mb-3'>
                    <Plus className='w-5 h-5 text-white' />
                 </div>
                 <p 
-                className='text-sm font-medium text-slate-700 text-center'>
+                className='text-sm font-medium text-slate-700 text-center cursor-pointer'>
                 Create Storey</p>
               </div>
           </div>
--- client/src/components/StoryModel.jsx
+++ client/src/components/StoryModel.jsx
@@ -1,11 +1,11 @@
-import { ArrowLeft } from 'lucide-react';
+import { ArrowLeft, TextIcon } from 'lucide-react';
 import React, { useState } from 'react'
 
 const StoryModel = ({setShowModal, fetchStories }) => {
 
 
   const bgColors = ["#4f46e5", "#7c3aed", "#db2777", "#e11d48", "#ca8a04", "#0d9488"]
-  const [mode, setDode] = useState("text");
+  const [mode, setMode] = useState("text");
   const [background, setBackground] = useState(bgColors[0]);
   const [text, setText] = useState("");
   const [media, setMedia] = useState(null);
@@ -33,6 +33,33 @@ const StoryModel = ({setShowModal, fetchStories }) => {
             <h2 className='text-lg font-semibold'>Create Story</h2>
             <span className='w-10'></span>
           </div>
+           <div className='rounded-lg h-96 flex items-center justify-center relative' style={{backgroundColor: background}}>
+
+            {mode === 'text' && (
+              <textarea onChange={(e)=>setText(e.target.value)} value={text} placeholder="What's on your mind?"  className='bg-transparent text-white w-full h-full p-6 text-lg resize-none focus:outline-none'/>
+            )}
+              {
+                mode === 'menia' && previewUrl && (
+                  media?.type.startsWith('image') ? (
+                    <img src={previewUrl} alt="" className='object-contain max-h-full' />
+                  ) : (
+                    <video src={previewUrl} className='object-contain max-h-full' />
+                  )
+                )
+              }
+           </div>
+
+            <div className='flex mt-4 gap-2'>
+              {bgColors.map((color)=>(
+                <button key={color} className='w-6 h-6 rounded-full ring cursor-pointer' style={{backgroundColor: color}} onClick={()=> setBackground(color)}/>
+              ))}
+            </div>
+
+            <div className='flex gap-2 mt-4'>
+               <button onClick={()=> {setMode('text'); setMedia(null); setPreviewUrl(null)}} className={`flex-1 flex items-center justify-center gap-2 p-2 rounded ${mode === 'text' ? "bg-white text-black" : "bg-zinc-800"}`}>
+                 <TextIcon size={18} /> Text
+               </button>
+            </div>
        </div>
 
     </div>
