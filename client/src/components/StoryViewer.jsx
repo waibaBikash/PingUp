873bfe6d3553e8533b67c779186eb0a7358ccc26
waibaBikash <kavrelibikash@gmail.com>
2025-08-27 21:43:22 +1000

    feat: refactor StoryViewer to improve content rendering logic and structure

--- client/src/components/StoryViewer.jsx
+++ client/src/components/StoryViewer.jsx
@@ -6,6 +6,28 @@ const StoryViewer = ({viewStory, setViewStory}) => {
   const handleClose = ()=>{
     setViewStory(null)
   }
+
+  const renderContent = ()=>{
+    switch (viewStory.media_type) {
+      case 'image':
+         return(
+          <img src={viewStory.media_url} alt="" className='max-w-full max-h-screen object-contain' />
+         );
+      case 'video':
+         return(
+          <video onEnded={()=>setViewStory(null)} src={viewStory.media_url} className='max-h-screen' />
+         );
+      case 'text':
+         return(
+          <div className='w-full h-full flex items-center justify-center p-8 text-white text-2xl text-center'>
+            {viewStory.content}
+          </div>
+         );
+    
+      default:
+        return null;
+    }
+  }
   return (
     <div className='fixed inset-0 h-screen bg-black bg-opacity-90 z-110 flex items-center justify-center' style={{backgroundColor: viewStory.media_type === 'text' ? viewStory.background_color : '#000000'}}>
        {/* Progress Bar */}
@@ -29,7 +51,11 @@ const StoryViewer = ({viewStory, setViewStory}) => {
             {/* Closed Button  */}
             <button onClick={handleClose} className='absolute top-4 right-4 text-white text-3xl font-bold focus:outline-none'>
                <X className='w-8 h-8 hover:scale-110 transition cursor-pointer' />
-            </button>
+             </button>
+            {/*Content Wrapper */}
+            <div className='max-w-[90vw] max-h-[90vh] flex items-center justify-center'>
+                {renderContent()}
+            </div>
        </div>
   )
 }
