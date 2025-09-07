383519d82c86af4970a8323c81b6d90de69681e4
waibaBikash <kavrelibikash@gmail.com>
2025-09-07 22:42:02 +1000

    click functionality for profile and comment

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { dummyConnectionsData } from '../assets/assets'
+import { Eye, MessageSquare } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const Messages = () => {
+
+  const navigate = useNavigate()
   return (
     <div className='min-h-screen relative bg-slate-50'>
         <div className='max-w-6xl mx-auto p-6'>
@@ -21,6 +25,16 @@ const Messages = () => {
                          <p className='text-slate-500'>@{user.username}</p>
                          <p className='text-sm text-gray-600'>{user.bio}</p>
                       </div>
+
+                       <div className='flex flex-col gap-2 mt-4'>
+                          <button onClick={()=> navigate(`/message/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1'>
+                             <MessageSquare />
+                          </button>
+
+                          <button  onClick={()=> navigate(`/profile/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer'>
+                             <Eye />
+                          </button>
+                       </div>
                   </div>
                 ))}
              </div>
