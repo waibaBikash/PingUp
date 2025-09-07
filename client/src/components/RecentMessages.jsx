5c36689adff5b392b3dd05927d1f4d5469bd8024
waibaBikash <kavrelibikash@gmail.com>
2025-09-07 22:19:48 +1000

    complte message section

--- client/src/components/RecentMessages.jsx
+++ client/src/components/RecentMessages.jsx
@@ -20,13 +20,17 @@ const RecentMessages = () => {
          <div className='flex flex-col max-h-56 overflow-y-hidden no-scrollbar'>
            {
             Messages.map((message, index)=>(
-              <Link key={index} className='flex items-center gap-2 py-2 hover:bg-slate-100'>
+              <Link to={`/messages/${message.from_user_id._id}`} key={index} className='flex items-center gap-2 py-2 hover:bg-slate-100'>
                  <img src={message.from_user_id.profile_picture} alt="" className='w-8 h-8 rounded-full' />
                   <div className='w-full'>
-                    <div className='flex justify-between'>
+                     <div className='flex justify-between'>
                      <p className='font-medium'>{message.from_user_id.full_name}</p>
                      <p className='text-[10px] text-slate-400'>{moment(message.createdAt).fromNow()}</p>
-                  </div>
+                    </div>
+                      <div className='flex justify-between'>
+                         <p className='text-gray-500'>{message.text ? message.text : 'Media'}</p>
+                         {!message.seen && <p className='bg-indigo-500 text-white w-4 h-4 flex items-center justify-center rounded-full text-[10px]'>1</p>}
+                      </div>
                   </div>
                   
               </Link>
