d1ed3e29f340121b139450e6c112b97423c775ba
waibaBikash <kavrelibikash@gmail.com>
2025-09-07 21:58:24 +1000

    style: reorganize sponsored section in Feed component for improved layout

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets';
+import { assets, dummyPostsData } from '../assets/assets';
 import Loading from '../components/Loading';
 import StoriesBar from '../components/StoriesBar';
 import PostCard from '../components/PostCard';
@@ -30,9 +30,12 @@ const Feed = () => {
          </div>
       </div>
       {/* Right side bar */}
-       <div>
-         <div>
-          <h1>Sponsored</h1>
+       <div className='max-xl:hidden sticky top-0'>
+         <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+           <h3>Sponsored</h3>
+            <img src={assets.sponsored_img} alt="" className='w-75 h-50 rounded-md' />
+            <p className='text-slate-600'>Email marketing</p>
+            <p className='text-slate-400'>Superchage your marketing with a powerful, easy-to-use platform build for results.</p>
          </div>
           <h1>Recent Messages</h1>
        </div>
