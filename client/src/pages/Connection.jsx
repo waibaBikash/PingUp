f3f3999ac3451f77f06559ea87570f29e6defcb3
waibaBikash <kavrelibikash@gmail.com>
2025-09-14 22:54:44 +1000

    feat: add tab functionality to Connection component for better navigation

--- client/src/pages/Connection.jsx
+++ client/src/pages/Connection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Users, UserPlus, UserCheck, UserRoundPen, MessageSquare} from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 import {
@@ -9,6 +9,7 @@ import {
 
 const Connection = () => {
 
+  const [currentTab, setCurrentTab] = useState('Followers');
   const navigate = useNavigate();
 
   const dataArray = [
@@ -36,6 +37,21 @@ const Connection = () => {
                  </div>
                ))}
              </div>
+
+             {/* Tabs */}
+              <div className='inline-flex flex-wrap items-center border border-gray-200 rounded-md p-1 bg-white shadow-sm'>
+                 {
+                   dataArray.map((tab)=>(
+                    <button onClick={()=> setCurrentTab(tab.label)} key={tab.label} className={`flex items-center px-3 py-1 text-sm rounded-md transition-colors ${currentTab === tab.label ? 'bg-white font-medium text-black' : 'text-gray-500 hover:text-black'}`}>
+                        <tab.icon className='w-4 h-4' />
+                         <span className='ml-1'>{tab.label}</span>
+                         {tab.count !== undefined && (
+                          <span className='ml-2 text-xs bg-gray-100 text-gray-700 px-2 py-0.5 rounded-full'>{tab.count}</span>
+                         ) }
+                    </button>
+                   ))
+                 }
+              </div>
        </div>
     </div>
   )
